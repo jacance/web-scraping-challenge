5872312dd2b4e57dcf1a7b35a8cca15aadfe0d30
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-01-10 22:02:01 -0800

    scrape function added

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,14 +1,27 @@
 # import libraries
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+import scrape_mars
 
 # create instance of Flask app
 app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
 
 # create route that renders index.html template
 @app.route("/")
-def echo():
-    return render_template("index.html", text= "Mission to Mars")
+def home():
+    mars_data = mongo.db.mars_site.find_one()
+    print(mars_data)
+    return render_template("index.html", text= mars_data)
 
+@app.route("/scrape")
+def scrape():
+    # Run the scrape function
+    mars_dict = scrape_mars.scrape_info()
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_site.update({}, mars_dict, upsert=True)
+    # Redirect back to home page
+    return redirect("/")
+    
 if __name__ == "__main__":
     app.run(debug=True)
