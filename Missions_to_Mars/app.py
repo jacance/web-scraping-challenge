9368c18494e2116a7970c63b277ff42bb298668e
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-01-10 17:05:42 -0800

    bootstrap

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,5 +1,6 @@
 # import libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+# from flask_pymongo import PyMongo
 
 # create instance of Flask app
 app = Flask(__name__)
